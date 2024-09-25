945068b862ba5b70521bab3982846dcdae2b67aa
adityapanda2608@gmail.com <adityapanda2608@gmail.com>
2024-09-25 18:14:35 +0530

    first commit

---
+++ views/main/script.js
@@ -0,0 +1,8 @@
+//================================typing animation================================//
+
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "Web Developer", "Graphic Designer", "Youtuber"],
+    typeSpeed:150,
+    BackSpeed:100,
+    loop:true
+})
